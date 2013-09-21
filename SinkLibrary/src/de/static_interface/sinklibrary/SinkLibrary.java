b94ea6d955df36176ae534524668e6d31d5dbb8e
Trojaner <Trojaner25@gmail.com>
2013-09-21 19:13:20 +0300

    Fixed bukkit classpath error, added SinkLibrary

---
+++ SinkLibrary/src/de/static_interface/sinklibrary/SinkLibrary.java
@@ -0,0 +1,20 @@
+package de.static_interface.sinklibrary;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Author: Trojaner
+ * Date: 21.09.13
+ */
+public class SinkLibrary extends JavaPlugin
+{
+    public void onEnable()
+    {
+
+    }
+
+    public void onDisable()
+    {
+
+    }
+}
