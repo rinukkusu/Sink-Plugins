45cb59ec179a18b27a64f8d80c229de4093dabeb
Trojaner <Trojaner25@gmail.com>
2013-09-26 21:22:34 +0300

    Removed PermissionsEx jar

---
+++ SinkLibrary/src/de/static_interface/sinklibrary/PermissionsNotAvailableException.java
@@ -0,0 +1,9 @@
+package de.static_interface.sinklibrary;
+
+/**
+ * Author: Trojaner
+ * Date: 26.09.13
+ */
+public class PermissionsNotAvailableException
+{
+}
---
+++ SinkLibrary/src/de/static_interface/sinklibrary/exceptions/PermsNotAvailableException.java
@@ -0,0 +1,13 @@
+package de.static_interface.sinklibrary.exceptions;
+
+/**
+ * Author: Trojaner
+ * Date: 26.09.13
+ */
+public class PermissionsNotAvailableException extends Exception
+{
+    public PermissionsNotAvailableException()
+    {
+        super("Permissions are not available!");
+    }
+}
