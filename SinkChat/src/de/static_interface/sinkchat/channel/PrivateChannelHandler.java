f2230981a731a0a1d7154e2c65575ee11f10ab94
Shadow1Raven <itsmee.minecraft@gmail.com>
2013-10-14 23:58:24 +0200

    Added content to PrivateChannelHandler.java

--- SinkChat/src/de/static_interface/sinkchat/channel/PrivateChannelHandler.java
+++ SinkChat/src/de/static_interface/sinkchat/channel/PrivateChannelHandler.java
@@ -1,4 +1,19 @@
 package de.static_interface.sinkchat.channel;
 
+import java.util.TreeMap;
+
 public class PrivateChannelHandler {
+
+    private static TreeMap<String, IPrivateChannel> registeredConversations = new TreeMap<>();
+
+    public void registerChannel(String channelIdentifier, IPrivateChannel channel)
+    {
+        registeredConversations.put(channelIdentifier, channel);
+    }
+
+    public IPrivateChannel getChannel(String channelIdentifier)
+    {
+        return (registeredConversations.get(channelIdentifier));
+    }
+
 }
