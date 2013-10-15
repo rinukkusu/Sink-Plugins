daab4663240ffc389ff8ef22997001257d34ae3a
Trojaner <Trojaner25@gmail.com>
2013-10-15 17:45:12 +0300

    Optimized DrugDeadListener by using User class

--- SinkCommands/src/de/static_interface/sinkcommands/listener/DrugDeadListener.java
+++ SinkCommands/src/de/static_interface/sinkcommands/listener/DrugDeadListener.java
@@ -17,7 +17,10 @@
 package de.static_interface.sinkcommands.listener;
 
 import de.static_interface.sinkcommands.commands.DrugCommand;
+import de.static_interface.sinklibrary.SinkLibrary;
+import de.static_interface.sinklibrary.User;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -30,9 +33,10 @@ public class DrugDeadListener implements Listener
     @EventHandler(priority = EventPriority.NORMAL)
     public void onPlayerDeath(PlayerDeathEvent event)
     {
-        if ( event.getEntity().equals(DrugCommand.killedByDrugs) )
+        if ( event.getEntity().equals(DrugCommand.killedByDrugs) && event.getEntityType() == EntityType.PLAYER )
         {
-            event.setDeathMessage(DrugCommand.PREFIX + ChatColor.RED + event.getEntity().getDisplayName() + ChatColor.WHITE + " nahm zu viele Drogen und ist deswegen gestorben.");
+            User user = SinkLibrary.getUser(event.getEntity());
+            event.setDeathMessage(DrugCommand.PREFIX + ChatColor.RED + user.getDisplayName() + ChatColor.WHITE + " nahm zu viele Drogen und ist deswegen gestorben.");
             DrugCommand.killedByDrugs.removePotionEffect(PotionEffectType.BLINDNESS);
             DrugCommand.killedByDrugs.removePotionEffect(PotionEffectType.CONFUSION);
             DrugCommand.killedByDrugs = null;
